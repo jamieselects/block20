585290294db26e74b902065a3fbc63f7b5dcd5bc
James Barnes Jr <jamie@mac.mynetworksettings.com>
2025-01-15 23:51:45 -0500

    Workshop updates

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// TODO: this file! :)
+//definte the starting number arrays
+const numberBank = [];
+const odds = [];
+const evens = [];
+
+//define all the document objects we're going to use
+const form = document.querySelector("form");
+const numberInput = document.getElementById("number");
+console.log("numberInput is",numberInput);
+const numberBankNumbers = document.getElementById("numberBankNumbers");
+const sortOneButton = document.getElementById("sortOneButton");
+const sortAllButton = document.getElementById("sortAllButton");
+const oddsBank = document.getElementById("oddsBank");
+const evensBank = document.getElementById("evensBank");
+
+//create event listener function for form submission
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //convert form submission string to number
+  const numberValue = numberInput.value.trim();
+  const numberToAdd = Number(numberValue);
+  numberBank.push(numberToAdd);
+
+  //call update numberBank function to update the doc object
+  updateNumberBank();
+  numberInput.value = "";
+});
+
+//funtion to update the numberBank
+const updateNumberBank = () => {
+  numberBankNumbers.innerHTML = "";
+  numberBankNumbers.textContent = numberBank.join(", ");
+}
+
+//function to update the evens / odds
+const updateSortedNumbers = () => {
+  oddsBank.textContent = odds.join(", ");
+  evensBank.textContent = evens.join(", ");
+}
+
+//sort 1 button functionality
+sortOneButton.addEventListener("click", (event) => {
+  const currentNumber = numberBank.shift();
+  if (currentNumber % 2 !== 0) {
+    odds.push(currentNumber);
+  } else {
+    evens.push(currentNumber);
+  }
+
+  updateNumberBank();
+  updateSortedNumbers();
+})
+
+//sort all button functionality
+sortAllButton.addEventListener("click", (event) => {
+  while (numberBank.length > 0) {
+    const number = numberBank.shift();
+
+    if (number % 2 !== 0) {
+      odds.push(number);
+    } else {
+      evens.push(number);
+    }
+  }
+
+  updateNumberBank();
+  updateSortedNumbers();
+})
+
+
+  
+
